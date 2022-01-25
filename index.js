5a46cd192d240a113265b8a00a90a4bc4b29435f
jacksonnorris <jacksonnorr@gmail.com>
2022-01-25 18:46:48 -0500

    init commit

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 let weeklyExpenses = 0
 let monthlyExpenses = 0
 let annualExpenses = 0
+let answer = 0
 
 // Your Code Here
+let expenseArray = [weeklyExpenseQuestions, monthlyExpenseQuestions, annualExpenseQuestions]
+let totalArray = [0, 0, 0]
+for (let i = 0; i < expenseArray.length; i++) {
+    // let total = 0
+    for (let j = 0; j < expenseArray[i].length; j++) {
+        answer = parseFloat(window.prompt(expenseArray[i][j]))
+        totalArray[i] += answer
+        // total += answer
+        // console.log(totalArray[i] + ", " + answer)
+    }
+    // totalArray[i] = total
+}
+weeklyExpenses = totalArray[0];
+monthlyExpenses = totalArray[1];
+annualExpenses = totalArray[2];
